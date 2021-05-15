58b683e4cb3bd7b1106060f5259197d864e1a9a2
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-05-15 10:56:20 -0500

    Addition of critical components

---
+++ src/components/DataArea/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import DataTable from "../dataTable/index";
+import Nav from "../Nav/index";
+import API from "../utils/API";
+
+
+export default class DataArea extends Component{
+
+    
+}
+
